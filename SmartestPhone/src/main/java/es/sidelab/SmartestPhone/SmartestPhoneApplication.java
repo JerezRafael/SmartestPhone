a002285468157bb3ae935f5d781b7ee51fb86b9c
David Pleite <pixapleite94@gmail.com>
2018-03-22 12:54:30 +0100

    Implementada la caché por completo

--- SmartestPhone/src/main/java/es/sidelab/SmartestPhone/SmartestPhoneApplication.java
+++ SmartestPhone/src/main/java/es/sidelab/SmartestPhone/SmartestPhoneApplication.java
@@ -1,12 +1,28 @@
 package es.sidelab.SmartestPhone;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 
+@EnableCaching
 @SpringBootApplication
 public class SmartestPhoneApplication {
+	
+	private static final Log LOG = LogFactory.getLog(SmartestPhoneApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartestPhoneApplication.class, args);
 	}
+	
+	@Bean("smartphones")
+	public CacheManager cacheManagerSmartphones() {
+		LOG.info("Activating cache...");
+		return new ConcurrentMapCacheManager("smartphones");
+	}
+	
 }
--- SmartestPhone/src/main/java/es/sidelab/SmartestPhone/repositorios/SmartphoneRepository.java
+++ SmartestPhone/src/main/java/es/sidelab/SmartestPhone/repositorios/SmartphoneRepository.java
@@ -2,18 +2,29 @@ package es.sidelab.SmartestPhone.repositorios;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.sidelab.SmartestPhone.clases.Smartphone;
 
+@CacheConfig(cacheManager="smartphones", cacheNames="smartphones")
 public interface SmartphoneRepository extends JpaRepository<Smartphone, Long> {
 
+	@SuppressWarnings("unchecked")
+	@CacheEvict(allEntries=true)
+	Smartphone save(Smartphone smartphone);
+	
+	@Cacheable
 	List<Smartphone> findByMarcaAndModelo(String marca, String modelo);
-
+	@Cacheable
 	Smartphone findByIdSmartphone(long idSmartphone);
-
+	@Cacheable
 	List<Smartphone> findByCamaraIdCamara(long idCamara);
-
+	@Cacheable
 	List<Smartphone> findByProcesadorIdProcesador(long idProcesador);
+	@Cacheable
+	List<Smartphone> findAll();
 
 }
